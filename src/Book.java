199dba84a2c871910c25459bd9ec7fc83096cbf1
KacperTetela1 <kacper.tetela@student.wsb.edu.pl>
2023-05-16 23:58:03 +0200

    dodaje metode rent i return oraz toS

--- src/Book.java
+++ src/Book.java
@@ -3,4 +3,19 @@ public class Book {
     private String author;
     private int publicationDate;
     private boolean availability;
+    public void rent(){
+        this.availability = false;
+    }
+    public void rentReturn(){
+        this.availability = true;
+    }
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", publicationDate=" + publicationDate +
+                ", availability=" + availability +
+                '}';
+    }
 }
