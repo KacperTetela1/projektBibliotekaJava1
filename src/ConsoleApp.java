66197307c9fc0a09cd6b0e798c6074b90ec38efb
KacperTetela1 <kacper.tetela@student.wsb.edu.pl>
2023-05-21 21:52:52 +0200

    dodaje pelna funkcjonalnosc do add book oraz add ebook

--- src/ConsoleApp.java
+++ src/ConsoleApp.java
@@ -17,10 +17,12 @@ public class ConsoleApp {
         while (running) {
             System.out.println("Select an option:");
             System.out.println("1. Add a book");
-            System.out.println("2. Delete a book");
-            System.out.println("3. Display book list");
-            System.out.println("4. Borrow a book");
-            System.out.println("5. Return a book");
+            System.out.println("2. Add a e-book");
+            System.out.println("3. Add a audio-book");
+            System.out.println("4. Delete a book");
+            System.out.println("5. Display book list");
+            System.out.println("6. Borrow a book");
+            System.out.println("7. Return a book");
             System.out.println("0. Exit the library");
 
             int option = scanner.nextInt();
@@ -79,12 +81,68 @@ public class ConsoleApp {
                     szkolna.addArt(book);
                     break;
                 case 2:
-                    //displayBooks();
+                    EBook eBook = new EBook();
+
+                    System.out.println("Please enter the title\n");
+                    String titleOfEBook = scanner.nextLine();
+                    eBook.setTitle(titleOfEBook);
+
+                    System.out.println("Please enter the author\n");
+                    String authorOfEbook = scanner.nextLine();
+                    eBook.setAuthor(authorOfEbook);
+
+                    System.out.println("Please enter the year of publication\n");
+                    int yearPublishedOfTheEbook = scanner.nextInt();
+                    eBook.setYearPublished(yearPublishedOfTheEbook);
+
+                    System.out.println("Please enter the language of ebook" +
+                            "\n1. Polish" +
+                            "\n2. English" +
+                            "\n3. German");
+                    int tmpValOfEbook = scanner.nextInt();
+                    switch (tmpValOfEbook){
+                        case 1:
+                            eBook.setLanguage(LiteraryArt.Language.POLISH);
+                            break;
+                        case 2:
+                            eBook.setLanguage(LiteraryArt.Language.ENGLISH);
+                            break;
+                        case 3:
+                            eBook.setLanguage(LiteraryArt.Language.GERMAN);
+                            break;
+                        default:
+                            System.out.println("Language has not been set");
+                    }
+
+                    System.out.println("Please enter the size of eBook file (MB)");
+                    int size = scanner.nextInt();
+                    eBook.setFileSize(size);
+
+                    szkolna.addArt(eBook);
                     break;
                 case 3:
-                    //borrowBook();
+                    AudioBook audioBook = new AudioBook();
+
+
+
+
+
+
+
+
+
+
                     break;
                 case 4:
+                    //Delete a book();
+                    break;
+                case 5:
+                    //display book list();
+                    break;
+                case 6:
+                    //borrow a book();
+                    break;
+                case 7:
                     //returnBook();
                     break;
                 case 0:
