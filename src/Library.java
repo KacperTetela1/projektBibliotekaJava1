9bc8d83533350b47e4fe5ff21fa6f68763a9b583
KacperTetela1 <kacper.tetela@student.wsb.edu.pl>
2023-05-21 20:33:43 +0200

    dodaje kolejne funkcjonalnoÅsci do klasy Library

--- src/Library.java
+++ src/Library.java
@@ -5,4 +5,27 @@ public class Library {
     public Library() {
         literaryArts = new ArrayList<>();
     }
+    public void addArt(LiteraryArt literaryArt){
+        literaryArts.add(literaryArt);
+    }
+    public void deleteArt(LiteraryArt literaryArt){
+        literaryArt = null;
+    }
+    public void printAllArts() {
+        int tmpVal = 1;
+        for (LiteraryArt literaryArt : literaryArts) {
+            System.out.print("Index: " + tmpVal + " ");
+            System.out.println(literaryArt.toString());
+        }
+    }
+    public void rentAnArt(LiteraryArt literaryArt){
+        if (literaryArt.isAvailability() == true){
+            literaryArt.setAvailability(false);
+            System.out.println("The art '" + literaryArt.getTitle() + "' has been rented");
+        }
+        else{
+            System.out.println("The art '" + literaryArt.getTitle() + "' has NOT been rented due to no availability");
+        }
+    }
+
 }
